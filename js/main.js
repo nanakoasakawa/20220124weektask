7153b7dcdc5e34047672d7a4edeadd54abfe60da
gem-yours <urgems000@gmail.com>
2022-01-24 19:47:08 +0900

    変数名をわかりやすくした

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-const target = document.getElementById("menu");
-target.addEventListener('click', () => {
-  const target = document.getElementById("menu");
-  target.classList.toggle('open');
+const menu = document.getElementById("menu");
+menu.addEventListener('click', (event) => {
+  menu.classList.toggle('open');
   const nav = document.getElementById("drawer-nav");
   nav.classList.toggle('in');
 });
